e50dcaa945768d3c79a14d7436eb3dbcb1f13cc8
DavisVaughan <davis@rstudio.com>
2022-01-11 14:40:58 -0500

    Add docs

--- R/generic.R
+++ R/generic.R
@@ -1,8 +1,23 @@
+#' Foo
+#'
+#' @param x Param
+#'
+#' @param ... Dots
+#'
+#' @param new New
+#'
 #' @export
 foo <- function(x, ..., new = NULL) {
   UseMethod("foo")
 }
 
+#' Bar
+#'
+#' @param x Param
+#'
+#' @param ... Dots
+#'
+#' @param new New
 #' @export
 bar <- function(x, ..., new = NULL) {
   UseMethod("bar")
